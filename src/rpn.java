7ef6c823c1994fe466cfd631f226c35d6e3c7176
panos <koskait1@hotmail.com>
2024-02-06 00:31:55 +0200

    if statements gia an pernaei h synarthsh gia input

--- src/rpn.java
+++ src/rpn.java
@@ -1,12 +1,19 @@
 public class rpn {
-    rpn(){
-       
-        
-            Object[] array = new Object[5];
-        array[1]='+';
-        array[0]=43;
-        System.out.println(array[1]);
-        System.out.println(array[0]);
+    rpn(Object[] n){
+
+        int numNum=0;
+        int numCh=0;
+       Object[] array = new Object[5];
+       for(int i=0;i<n.length;i++){
+        array[i]=n[i];
+       }
+       for(int i=0;i<array.length;i++){
+        if((int)array[i]==1 || (int)array[i]==2 ||(int)array[i]==3 || (int)array[i]==4 || (int)array[i]==5 || (int)array[i]==6 || (int)array[i]==7 || (int)array[i]==8 || (int)array[i]==9 || (int)array[i]==0){
+            numNum++;
+        }
+        if((char)array[i]=='+'||(char)array[i]=='-'||(char)array[i]=='*'||(char)array[i]=='/'||(char)array[i]=='^')
+            numCh++;
+        }
        
     }
     
