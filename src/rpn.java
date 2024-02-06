3373fab7301653ada199e9e2d82990cddef80aea
panos <koskait1@hotmail.com>
2024-02-06 21:51:25 +0200

    ασασ

--- src/rpn.java
+++ src/rpn.java
@@ -1,14 +1,15 @@
 public class rpn {
-    rpn(Object[] n){
-
+     rpn(Object[] n){
+        int p=0;
         int numNum=0;
         int numCh=0;
        Object[] array = new Object[5];
        for(int i=0;i<n.length;i++){
         array[i]=n[i];
        }
+       //elegxos gia to an to input einai apodekto.
        for(int i=0;i<array.length;i++){
-        if((int)array[i]==1 || (int)array[i]==2 ||(int)array[i]==3 || (int)array[i]==4 || (int)array[i]==5 || (int)array[i]==6 || (int)array[i]==7 || (int)array[i]==8 || (int)array[i]==9 || (int)array[i]==0){
+        if((int)array[i]>=0){
             numNum++;
         }
         if((char)array[i]=='+'||(char)array[i]=='-'||(char)array[i]=='*'||(char)array[i]=='/'||(char)array[i]=='^')
@@ -16,7 +17,17 @@ public class rpn {
         }
        if(numNum!=numCh+1){
         System.out.println("ta egrapses xalia");
-       }
+        }
+       for(int i=0;i<array.length-1;i++){
+        if((int)array[1]>=0 && (int)array[i+1]>=0){
+
+        } 
+    
+        }
+
+
+
+
     }
     
     
