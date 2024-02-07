7280672e00b49fda1aea1c0a3872e41362ec345d
panos <koskait1@hotmail.com>
2024-02-07 19:06:32 +0200

    TELOS? POGPOG

--- src/rpn.java
+++ src/rpn.java
@@ -7,36 +7,45 @@ public class rpn {
        for(int i=0;i<n;i++){
         array[i]=0;
        }
-     
-
-
     }
      
      int calculator(Object[] n){
         int p=0;
-        for(int i=0; i<n.length;i++){
-            array[i]=n[i];
-        }
         
-       
-        //
         if(n.length == 1 ){
             return (int)n[0];
         }
-           for(int i=0;i<n.length;i++){
+           for(int i=0;i<n.length-2;i++){
             
-            if (n[i] instanceof Integer && n[i + 1] instanceof Integer && n[i + 2] instanceof Character && (char) n[i + 2] == '+') {
+            if (n[i] instanceof Integer && n[i + 1] instanceof Integer && n[i + 2] instanceof Character && ((char) n[i + 2] == '+' ||(char) n[i + 2] == '-'||(char) n[i + 2] == '*'||(char) n[i + 2] == '/'))  {
+            ////////////////////symbol check////////////////////////////////
+                if(n[i+2] instanceof Character && ((char)n[i+2] == '+')){
                 p=(int)n[i]+(int)n[i+1];
+                }
+                if(n[i+2] instanceof Character && ((char)n[i+2] == '-')){
+                    p=(int)n[i]-(int)n[i+1];
+                    }
+                if(n[i+2] instanceof Character && ((char)n[i+2] == '*')){
+                    p=(int)n[i]*(int)n[i+1];
+                    }
+                if( n[i+2] instanceof Character && ((char)n[i+2] == '/')){
+                    if(n[i+1] instanceof Integer && ((int)n[i+1] == 0)){
+                        break;
+                    }
+                    p=(int)n[i]/(int)n[i+1];
+                    }
+            ///////////////////////////////////////////////////////////////
                 Object[] temparray = new Object[n.length-2];
                     for(int j=0;j<i;j++){
                         temparray[j]=n[j];
                     }
-                    temparray[i]=p;
+                 temparray[i]=p;
                     for(int j=i+3;j<n.length;j++){
                         temparray[j-2]=n[j];
                 }
                     return calculator(temparray);
             }
+            
             if(n[i] instanceof Character && n[i+1] instanceof Character && ((char)n[i]=='+'||(char)n[i+1]=='+')){
                 System.out.println("your input is wrong");
                 break;
@@ -48,8 +57,8 @@ public static void main(String[] args) throws Exception {
     Object[] test = new Object[5];
     test[0] = 1;
     test[1] = 1;
-    test[2] = 12;
-    test[3] = '+';
+    test[2] = 7;
+    test[3] = '/';
     test[4] = '+';
     System.out.println(test.length);
     System.out.println(test[4] instanceof Integer);
