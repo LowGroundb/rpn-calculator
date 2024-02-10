80f5ae628252a0b2710a147bb9de6b34049a4d67
panos <koskait1@hotmail.com>
2024-02-10 18:03:29 +0200

    cleaner code

--- src/rpn.java
+++ src/rpn.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;  
+
 public class rpn {
     private Object[] array;
     
@@ -9,60 +9,81 @@ public class rpn {
         array[i]=0;
        }
     }
+    public void setObjects(Object[] n){
+        this.array = n;
+    }
      
-     static int calculator(Object[] n){
-        int p=0;
+      int Calculator(){
+        int result=0;
         
-        if(n.length == 1 && n[0] instanceof Integer ){
-           
-            return (int)n[0];
-        }
-        
-           for(int i=0;i<n.length-2;i++){
+        for(int i=0;i<array.length-2;i++){
             
-            if (n[i] instanceof Integer && n[i + 1] instanceof Integer && n[i + 2] instanceof Character && ((char) n[i + 2] == '+' ||(char) n[i + 2] == '-'||(char) n[i + 2] == '*'||(char) n[i + 2] == '/'))  {
-            ////////////////////symbol check////////////////////////////////
-                if(n[i+2] instanceof Character && ((char)n[i+2] == '+')){
-                p=(int)n[i]+(int)n[i+1];
+            if (array[i] instanceof Integer && array[i + 1] instanceof Integer && array[i + 2] instanceof Character && ((char) array[i + 2] == '+' ||(char) array[i + 2] == '-'||(char) array[i + 2] == '*'||(char) array[i + 2] == '/'))  {
+                    //////////////////////////////symbol check/////////////////////////////////////
+                if(array[i+2] instanceof Character && ((char)array[i+2] == '+')){
+
+                    result=(int)array[i]+(int)array[i+1];
+                
                 }
-                if(n[i+2] instanceof Character && ((char)n[i+2] == '-')){
-                    p=(int)n[i]-(int)n[i+1];
-                    }
-                if(n[i+2] instanceof Character && ((char)n[i+2] == '*')){
-                    p=(int)n[i]*(int)n[i+1];
-                    }
-                if( n[i+2] instanceof Character && ((char)n[i+2] == '/')){
-                    if(n[i+1] instanceof Integer && ((int)n[i+1] == 0)){
-                        break;
-                    }
-                    p=(int)n[i]/(int)n[i+1];
-                    }
-            ///////////////////////////////////////////////////////////////
-                Object[] temparray = new Object[n.length-2];
-                    for(int j=0;j<i;j++){
-                        temparray[j]=n[j];
-                    }
-                 temparray[i]=p;
-                    for(int j=i+3;j<n.length;j++){
-                        temparray[j-2]=n[j];
+
+                if(array[i+2] instanceof Character && ((char)array[i+2] == '-')){
+
+                    result=(int)array[i]-(int)array[i+1];
+                    
                 }
-                    return calculator(temparray);
+                if(array[i+2] instanceof Character && ((char)array[i+2] == '*')){
+
+                    result=(int)array[i]*(int)array[i+1];
+                    
+                }
+                if( array[i+2] instanceof Character && ((char)array[i+2] == '/')){
+
+                    if(array[i+1] instanceof Integer && ((int)array[i+1] == 0)){break;}
+                    
+                    result=(int)array[i]/(int)array[i+1];
+                    
+                }
+                    ///////////////////////////////////////////////////////////////////
+                    //////////creation of the new array after the operation////////////
+                rpn temprpn = new rpn(array.length-3);
+                Object[] temparray = new Object[array.length-2];
+                    
+                for(int j=0;j<i;j++){ temparray[j]=array[j];}
+
+                temparray[i]=result;
+
+                for(int j=i+3;j<array.length;j++){temparray[j-2]=array[j];}
+                    
+                temprpn.setObjects(temparray);
+                return temprpn.Calculator();
+                   ///////////////////////////////////////////////////////////////////
             }
             
-            if(n[i] instanceof Character && n[i+1] instanceof Character && ((char)n[i]=='+'||(char)n[i+1]=='+')){
+            if(array[i] instanceof Character && array[i+1] instanceof Character && ((char)array[i]=='+'||(char)array[i+1]=='+')){
                 System.out.println("your input is wrong");
-                break;
+                    break;
             }
     }
-            return 0;
+        if(array.length == 1 && array[0] instanceof Integer ){return (int)array[0];}
+         return 0;
 }
 public static void main(String[] args) throws Exception {
-    Object[] test = new Object[5];
-    test = new Object[1];
+    Object[] test = new Object[11];
+    test[0] = 1;
+    test[1] = 1;
+    test[2] = '+';
+    test[3] = 1;
+    test[4] = 1;
+    test[5] = 1;
+    test[6] = '+';
+    test[7] = 1;
+    test[8] = '+';
+    test[9] = '*';
+    test[10] = '+';
     System.out.println(test.length);
-   
     rpn peos = new rpn(5);
-    int result = peos.calculator(test);
+    peos.setObjects(test);
+    int result = peos.Calculator();
     System.out.println("Result: " + result);
 }
 }
--- src/tempCodeRunnerFile.java
+++ src/tempCodeRunnerFile.java
@@ -1,1 +1,74 @@
-array
+public class rpn {
+    private Object[] array;
+    
+    
+    public rpn(int n){
+        array = new Object[n];
+       for(int i=0;i<n;i++){
+        array[i]=0;
+       }
+    }
+    public void setter(Object[] n){
+        this.array = n;
+    }
+     
+      int calculator(){
+        int p=0;
+        
+        if(array.length == 1 && array[0] instanceof Integer ){
+           
+            return (int)array[0];
+        }
+        
+           for(int i=0;i<array.length-2;i++){
+            
+            if (array[i] instanceof Integer && array[i + 1] instanceof Integer && array[i + 2] instanceof Character && ((char) array[i + 2] == '+' ||(char) array[i + 2] == '-'||(char) array[i + 2] == '*'||(char) array[i + 2] == '/'))  {
+            ////////////////////symbol check////////////////////////////////
+                if(array[i+2] instanceof Character && ((char)array[i+2] == '+')){
+                p=(int)array[i]+(int)array[i+1];
+                }
+                if(array[i+2] instanceof Character && ((char)array[i+2] == '-')){
+                    p=(int)array[i]-(int)array[i+1];
+                    }
+                if(array[i+2] instanceof Character && ((char)array[i+2] == '*')){
+                    p=(int)array[i]*(int)array[i+1];
+                    }
+                if( array[i+2] instanceof Character && ((char)array[i+2] == '/')){
+                    if(array[i+1] instanceof Integer && ((int)array[i+1] == 0)){
+                        break;
+                    }
+                    p=(int)array[i]/(int)array[i+1];
+                    }
+            ///////////////////////////////////////////////////////////////
+                Object[] temparray = new Object[array.length-2];
+                    for(int j=0;j<i;j++){
+                        temparray[j]=array[j];
+                    }
+                 temparray[i]=p;
+                    for(int j=i+3;j<array.length;j++){
+                        temparray[j-2]=array[j];
+                }
+                    return calculator();
+            }
+            
+            if(array[i] instanceof Character && array[i+1] instanceof Character && ((char)array[i]=='+'||(char)array[i+1]=='+')){
+                System.out.println("your input is wrong");
+                break;
+            }
+    }
+            return 0;
+}
+public static void main(String[] args) throws Exception {
+    Object[] test = new Object[5];
+    test[0] = 1;
+    test[1] = 1;
+    test[2] = '+';
+    test[3] = 12;
+    test[4] = '+';
+    System.out.println(test.length);
+    rpn peos = new rpn(5);
+    peos.setter(test);
+    int result = peos.calculator();
+    System.out.println("Result: " + result);
+}
+}
