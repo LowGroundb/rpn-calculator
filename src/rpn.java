3e275f9af073f642b2bb3584c4a261e07875059a
panos <koskait1@hotmail.com>
2024-02-10 19:45:10 +0200

    even better code

--- src/rpn.java
+++ src/rpn.java
@@ -3,56 +3,50 @@ public class rpn {
     private Object[] array;
     
     
-    public rpn(int n){
-        array = new Object[n];
-       for(int i=0;i<n;i++){
-        array[i]=0;
-       }
-    }
-    public void setObjects(Object[] n){
-        this.array = n;
-    }
+    public rpn(int size){array = new Object[size];}
+
+
+    public void setObjects(Object[] array){this.array = array;}
+
      
-      int Calculator(){
+    int Calculator(){
         int result=0;
         
         for(int i=0;i<array.length-2;i++){
             
-            if (array[i] instanceof Integer && array[i + 1] instanceof Integer && array[i + 2] instanceof Character && ((char) array[i + 2] == '+' ||(char) array[i + 2] == '-'||(char) array[i + 2] == '*'||(char) array[i + 2] == '/'))  {
+            if (array[i] instanceof Integer && array[i + 1] instanceof Integer && array[i + 2] instanceof Character)  {
                     //////////////////////////////symbol check/////////////////////////////////////
-                if(array[i+2] instanceof Character && ((char)array[i+2] == '+')){
-
-                    result=(int)array[i]+(int)array[i+1];
-                
-                }
-
-                if(array[i+2] instanceof Character && ((char)array[i+2] == '-')){
-
-                    result=(int)array[i]-(int)array[i+1];
+                    char operation = (char)array[i+2];
+                    switch (operation) {
+                        case '+':
+                            result = (int)array[i]+(int)array[i+1];
+                            break;
+                        case '-':
+                            result = (int)array[i]-(int)array[i+1];
+                            break;
+                        case '*':
+                            result = (int)array[i]*(int)array[i+1];
+                            break;
+                        case '/':
+                            if((int)array[i+1]==0){System.out.println("division with 0 ");return 0;}
+                            result = (int)array[i]/(int)array[i+1];
+                            break;
                     
-                }
-                if(array[i+2] instanceof Character && ((char)array[i+2] == '*')){
-
-                    result=(int)array[i]*(int)array[i+1];
-                    
-                }
-                if( array[i+2] instanceof Character && ((char)array[i+2] == '/')){
-
-                    if(array[i+1] instanceof Integer && ((int)array[i+1] == 0)){break;}
-                    
-                    result=(int)array[i]/(int)array[i+1];
-                    
-                }
+                        default:
+                        System.out.println("wrong input");
+                        return 0;
+                            
+                    }
                     ///////////////////////////////////////////////////////////////////
                     //////////creation of the new array after the operation////////////
                 rpn temprpn = new rpn(array.length-3);
                 Object[] temparray = new Object[array.length-2];
                     
-                for(int j=0;j<i;j++){ temparray[j]=array[j];}
-
-                temparray[i]=result;
-
-                for(int j=i+3;j<array.length;j++){temparray[j-2]=array[j];}
+                for(int j=0;j<array.length-2;j++){
+                    temparray[j]=array[j];
+                    if(j==i){temparray[j]=result;}
+                    if(j>i){temparray[j]=array[j+2];}
+                }
                     
                 temprpn.setObjects(temparray);
                 return temprpn.Calculator();
@@ -65,12 +59,13 @@ public class rpn {
             }
     }
         if(array.length == 1 && array[0] instanceof Integer ){return (int)array[0];}
-         return 0;
+        System.out.println("wrong input"); 
+        return 0;
 }
 public static void main(String[] args) throws Exception {
     Object[] test = new Object[11];
     test[0] = 1;
-    test[1] = 1;
+    test[1] = 0;
     test[2] = '+';
     test[3] = 1;
     test[4] = 1;
@@ -78,9 +73,8 @@ public static void main(String[] args) throws Exception {
     test[6] = '+';
     test[7] = 1;
     test[8] = '+';
-    test[9] = '*';
+    test[9] = '+';
     test[10] = '+';
-    System.out.println(test.length);
     rpn peos = new rpn(5);
     peos.setObjects(test);
     int result = peos.Calculator();
