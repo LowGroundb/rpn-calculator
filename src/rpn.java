a42a8e714bae83d0a335defa1d0fa385a9627ce6
panos <koskait1@hotmail.com>
2024-02-14 01:33:20 +0200

    full better calculator

--- src/rpn.java
+++ src/rpn.java
@@ -2,7 +2,6 @@
 public class rpn {
     private Object[] array;
     
-    
     public rpn(int size){array = new Object[size];}
 
 
@@ -14,22 +13,28 @@ public class rpn {
         
         for(int i=0;i<array.length-2;i++){
             
-            if (array[i] instanceof Integer  a && array[i + 1] instanceof Integer  b && array[i + 2] instanceof Character)  {
+            if (array[i] instanceof Integer   && array[i + 1] instanceof Integer   && array[i + 2] instanceof Character)  {
                     //////////////////////////////symbol check/////////////////////////////////////
-                    char operation = (char)array[i+2];
+                    int a = (int) array[i];
+                    int b = (int) array[i + 1];
+                    char operation = (char) array[i + 2];
                     switch (operation) {
                         case '+':
                             result = a+b;
+                            
                             break;
                         case '-':
                             result = a-b;
+                           
                             break;
                         case '*':
                             result = a*b;
+                            
                             break;
                         case '/':
                             if(b==0){System.out.println("division with 0 ");return 0;}
                             result = a/b;
+                            
                             break;
                     
                         default:
@@ -37,30 +42,26 @@ public class rpn {
                         return 0;
                             
                     }
-                    ///////////////////////////////////////////////////////////////////
-                    //////////creation of the new array after the operation////////////
-                rpn temprpn = new rpn(array.length-3);
-                Object[] temparray = new Object[array.length-2];
-                    
-                for(int j=0;j<array.length-2;j++){
-                    temparray[j]=array[j];
-                    if(j==i){temparray[j]=result;}
-                    if(j>i){temparray[j]=array[j+2];}
-                }
-                    
-                temprpn.setObjects(temparray);
-                return temprpn.Calculator();
-                   ///////////////////////////////////////////////////////////////////
+                    creationOfNewArray(result,i);
+                    return result;
             }
             
-            if(array[i] instanceof Character && array[i+1] instanceof Character && ((char)array[i]=='+'||(char)array[i+1]=='+')){
-                System.out.println("your input is wrong");
-                    break;
-            }
     }
-        if(array.length == 1 && array[0] instanceof Integer ){return (int)array[0];}
+    if(array.length == 1 && array[0] instanceof Integer ){return (int)array[0];}
         System.out.println("wrong input"); 
         return 0;
 }
-
+void creationOfNewArray(int resultOfExpresion, int indexOfArray){
+    rpn tempRpnArray = new rpn(array.length-3);
+    Object[] tempArray = new Object[array.length-2];
+        
+    for(int j=0;j<tempArray.length;j++){
+        tempArray[j]=array[j];
+        if(j==indexOfArray){tempArray[j]=resultOfExpresion;}
+        if(j>indexOfArray){tempArray[j]=array[j+2];}
+    }
+        
+    tempRpnArray.setObjects(tempArray);
+     tempRpnArray.Calculator();
+}
 }
